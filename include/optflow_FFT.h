eca3e576ad0890c87ddd8a647a1383cfa343ed9a
Xu <1687701765@qq.com>
2020-10-05 15:00:20 +0800

    add phase optflow use fftw

---
+++ include/optflow_FFT.h
@@ -0,0 +1,33 @@
+#ifndef OPTFLOW_FFT_H
+#define OPTFLOW_FFT_H
+#include <stdint.h>
+#include <fftw3.h>
+#include <opencv2/core/core.hpp>
+using namespace cv;
+class optflow_FFT
+{
+    public:
+        optflow_FFT(uint32_t n);
+        virtual ~optflow_FFT();
+        void run(uint32_t n);
+        void fill_data(Mat &in, uint32_t x0, uint32_t y0);
+        void calc_delta();
+        void copy_result(uint8_t* p);
+
+    protected:
+        int save();
+        int load();
+
+    private:
+        uint32_t n=0;
+        fftw_plan p1;
+        fftw_plan p2;
+        fftw_plan p_ifft;
+        double *crop_db;
+        fftw_complex *out1;
+        fftw_complex *out2;
+        fftw_complex *mul;
+        double *ifft;
+};
+
+#endif // OPTFLOW_FFT_H
