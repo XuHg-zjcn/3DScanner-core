c297a92fbfd0dfe35824cf8e8e71ed443b088fb6
Xu <1687701765@qq.com>
2020-10-15 17:47:17 +0800

    move runHough code into a func

--- main.cpp
+++ main.cpp
@@ -11,9 +11,9 @@
 #include "optflow_FFT.h"
 using namespace std;
 using namespace cv;
-Hough *mHough;
-MT_para m_para;
-timespec ts0{}, ts1{};
+
+void runHough(Mat &gray, Mat &out);
+
 int main(int argc, char *argv[])
 {
     Mat img_color = imread("lena.jpg", CV_LOAD_IMAGE_COLOR);
@@ -28,19 +28,7 @@ int main(int argc, char *argv[])
     gray.create(img_color.size(), CV_8UC1);
     cvtColor(img_color, gray, CV_RGB2GRAY);
 
-    out.create(50, 50, CV_8UC1);
-    m_para.size_from_OpenCV_Mat(gray, out);
-    m_para.start.set_xy16(128, 128);
-    m_para.rads={start:0, step:pi/50};
-    m_para.N_length=50;
-    m_para.N_thread=1;
-    mHough = new Hough(&m_para);
-
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &ts0);
-    mHough->update(gray.ptr(), out.ptr());
-    mHough->waitOK();
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &ts1);
-    //cout<< (ts1.tv_nsec - ts0.tv_nsec)/1000 << "us" <<endl;
+    runHough(gray, out);
 
     show_wave.create(64, 33, CV_8UC1);
     show_ifft.create(64, 64, CV_8UC1);
@@ -63,4 +51,24 @@ int main(int argc, char *argv[])
 
     waitKey(0);
     return 0;
+}
+
+void runHough(Mat &gray, Mat &out)
+{
+    MT_para m_para;
+    timespec ts0{}, ts1{};
+    Hough *mHough;
+    out.create(50, 50, CV_8UC1);
+    m_para.size_from_OpenCV_Mat(gray, out);
+    m_para.start.set_xy16(128, 128);
+    m_para.rads={start:0, step:pi/50};
+    m_para.N_length=50;
+    m_para.N_thread=1;
+    mHough = new Hough(&m_para);
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &ts0);
+    mHough->update(gray.ptr(), out.ptr());
+    mHough->waitOK();
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &ts1);
+    //cout<< (ts1.tv_nsec - ts0.tv_nsec)/1000 << "us" <<endl;
 }
